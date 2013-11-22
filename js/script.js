1f73a501c7dc238cdbc2dfdc9f47fba24fff411f
Franky W. (franky - localhost) <franky@newrelic.com>
2013-11-22 14:51:29 -0800

    Global JS object called blankSlate

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
+window.blankSlate = {}
+
+/* Can call this function through console by typing blankSlate.helloWorld() */
+blankSlate.helloWorld = function(){ console.log("Hello World")}
+
 $(document).ready(function(){
   console.log(':)')
 })
